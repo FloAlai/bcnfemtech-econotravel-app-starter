5ec2166d1a69673b1d3a58144fffcec8dc9004ff
Ana Lissette <lissilab@hotmail.com>
2021-07-29 09:57:33 +0200

    arreglando conflicto en git

--- web-client/src/js/main.js
+++ web-client/src/js/main.js
@@ -4,17 +4,11 @@
 import '../scss/main.scss'
 import React from "react";
 import ReactDOM from "react-dom";
-<<<<<<< HEAD
+
 import {App} from "./components/App"
 import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
-
-=======
-import {App} from "./components/App";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap';
->>>>>>> a1b7b4ecf0a9c4770826b448087bad1179737f52
 ReactDOM.render(
     React.createElement(App),
     document.getElementById("app")
