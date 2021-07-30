0f2ebdb68dfdb0725f01a3b0fcaafc6d79d40bf2
Floalai <wikalai@gmail.com>
2021-07-30 14:21:32 +0200

    llegando a la presentacion

--- web-client/src/js/components/BootstrapCarousel.jsx
+++ web-client/src/js/components/BootstrapCarousel.jsx
@@ -15,8 +15,7 @@ import 'bootstrap/dist/css/bootstrap.min.css'
                     alt="First slide"
                 />
                 <Carousel.Caption>
-                    <h5>Montseni</h5>
-                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+
                 </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
@@ -26,8 +25,7 @@ import 'bootstrap/dist/css/bootstrap.min.css'
                     alt="Second slide"
                 />
                 <Carousel.Caption>
-                    <h5>Second slide label</h5>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+
                 </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
@@ -37,8 +35,7 @@ import 'bootstrap/dist/css/bootstrap.min.css'
                     alt="Third slide"
                 />
                 <Carousel.Caption>
-                    <h5>Third slide label</h5>
-                    <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+
                 </Carousel.Caption>
             </Carousel.Item>
         </Carousel>
